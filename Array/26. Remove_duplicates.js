a7103e584746a81fc3ffa37dbb088f7828229480
Nur A Alam <nuraalam376@gmail.com>
2025-06-08 20:23:50 +0600

    26. Remove duplicates

---
+++ Array/26. Remove_duplicates.js
@@ -0,0 +1,20 @@
+// 26. Remove Duplicates from Sorted Array
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const removeDuplicates = function(nums) {
+    let x = 0;
+
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] > nums[x]) {
+            x = x + 1;
+            nums[x] = nums[i];
+        }
+    }
+    return x + 1;
+};
+
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
+console.log(removeDuplicates([1,1,2]));
