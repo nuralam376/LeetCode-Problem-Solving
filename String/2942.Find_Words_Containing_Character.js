036ced178bc181aac5ff52f3be7d0a37e409f2be
Nur A Alam <nuraalam376@gmail.com>
2025-08-28 09:52:38 +0600

    2942. Find Words Containing Character

---
+++ String/2942.Find_Words_Containing_Character.js
@@ -0,0 +1,22 @@
+// 2942. Find Words Containing Character
+
+/**
+ * @param {string[]} words
+ * @param {character} x
+ * @return {number[]}
+ */
+var findWordsContaining = function(words, x) {
+  let result = [];
+  
+  for(let i = 0; i < words.length; i++) {
+    for(let j = 0; j < words[i].length; j++) {
+        if(words[i][j] === x) {
+            result.push(i);
+            break;
+        }
+    }
+  }
+  return result;
+};
+
+console.log(findWordsContaining(["leet","code"], "e"));
