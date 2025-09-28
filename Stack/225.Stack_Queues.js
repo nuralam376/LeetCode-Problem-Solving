eace57501be2acbde7fb45bf30e15ea4520e01b6
Nur A Alam <nuraalam376@gmail.com>
2025-09-28 10:14:32 +0600

    Implement stack using one queue

--- Stack/225.Stack_Queues.js
+++ Stack/225.Stack_Queues.js
@@ -62,3 +62,57 @@ var param_2 = obj.pop()
 var param_3 = obj.top()
 var param_4 = obj.empty()
  
+
+// 225. Implement Stack one Queue
+
+var MyStack = function() {
+  this.q = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+    this.q.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+   let n = this.q.length;
+
+   for(let i = 0; i < n - 1; i++) {
+        this.q.push(this.q.shift());
+   }
+   return this.q.shift();
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+    let n = this.q.length;
+
+    for(let i = 0; i < n - 1; i++) {
+        this.q.push(this.q.shift());
+    }
+    let front = this.q[0];
+    this.q.push(this.q.shift());
+    return front;
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+    return this.q.length === 0;
+};
+
+var obj = new MyStack()
+obj.push(5)
+var param_2 = obj.pop()
+var param_3 = obj.top()
+var param_4 = obj.empty()
+ 
