5b14cb78d3dc7abfd0471672002f7c94ce211b16
Nur A Alam <nuraalam376@gmail.com>
2025-10-26 10:26:54 +0600

    33. Search in Rotated Sorted Array

--- Binary Search/33.Rotated_Sorted_Array.js
+++ Binary Search/33.Rotated_Sorted_Array.js
@@ -6,31 +6,31 @@
  * @return {number}
  */
 const search = function(nums, target) {
-    let left = 0;
-    let right = nums.length - 1;
-    let mid;
+  let l = 0;
+  let r = nums.length - 1;
 
-    while(left <= right) {
-        mid = Math.floor((left + right) / 2);
+  while(l <= r) {
+    let m = l + Math.floor((r - l) / 2);
 
-        if(target === nums[mid]) {
-            return mid;
-        }
-        else if(nums[left] <= nums[mid]) {
-            if(target > nums[mid] || target < nums[left]) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+    if(target === nums[m]) {
+        return m;
+    }
+
+    if(nums[l] <= nums[m]) {
+        if(target >= nums[l] && target < nums[m]) {
+            r = m - 1;
         } else {
-            if(target < nums[mid] || target > nums[right]) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
+            l = m + 1;
+        }
+    } else {
+        if(target > nums[m] && target <= nums[r]) {
+            l = m + 1;
+        } else {
+            r = m - 1;
         }
     }
-    return -1;
+  }
+  return -1;
 };
 
 console.log(search([4,5,6,7,0,1,2], 0));
