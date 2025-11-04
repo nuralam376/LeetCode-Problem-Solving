5d2684ac9f58e2e5dbd8da33f8cd583cd495bfe9
Nur A Alam <nuraalam376@gmail.com>
2025-11-04 09:55:01 +0600

    852. Peak Index in a Mountain Array

--- Binary Search/852.Peak_Index.js
+++ Binary Search/852.Peak_Index.js
@@ -5,20 +5,19 @@
  * @return {number}
  */
 const peakIndexInMountainArray = function(arr) {
-    let low = 0;
-    let high = arr.length - 1;
-    let mid;
+    let l = 0;
+    let r = arr.length - 1;
 
-    while(low < high) {
-        mid = Math.floor((low + high) / 2);
+    while(l < r) {
+        let m = l + Math.floor((r - l) /2);
 
-        if(arr[mid] > arr[mid + 1]) {
-            high = mid;
+        if(arr[m+1] > arr[m]) {
+            l = m + 1;
         } else {
-            low = mid + 1;
+            r = m;
         }
     }
-    return low;
+    return l;
 };
 
 console.log(peakIndexInMountainArray([0,1,0]));
