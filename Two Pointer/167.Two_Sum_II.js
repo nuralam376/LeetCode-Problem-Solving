bc437ae0f26fe2e3b21517ab964c4ff0e334d1ab
Nur A Alam <nuraalam376@gmail.com>
2025-11-10 09:50:54 +0600

    167. Two Sum II - Input Array Is Sorted

--- Two Pointer/167.Two_Sum_II.js
+++ Two Pointer/167.Two_Sum_II.js
@@ -1,26 +1,25 @@
 // 167. Two Sum II - Input Array Is Sorted
+
 /**
  * @param {number[]} numbers
  * @param {number} target
  * @return {number[]}
  */
-const twoSum = function(numbers, target) {
-    let left = 0;
-    let right = numbers.length - 1;
+var twoSum = function(numbers, target) {
+    let i = 0;
+    let j = numbers.length - 1;
+    
+    while(i < j) {
+        let sum = numbers[i] + numbers[j];
 
-    while(left < right) {
-        let currentSum = numbers[left] + numbers[right];
-        if(currentSum > target) {
-            right = right - 1;
-        } else if(currentSum < target){
-            left = left + 1;
+        if(sum > target) {
+            --j;
+        } else if(sum < target) {
+            i++;
         } else {
-         return [left + 1, right + 1];   
+            return [i + 1, j + 1];
         }
     }
-    return [];
 };
 
-console.log(twoSum([2,7,11,15], 9));
-console.log(twoSum([2,3,4], 6));
-console.log(twoSum([-1,0], -1));
+console.log(twoSum([2,7,11,15],9));
