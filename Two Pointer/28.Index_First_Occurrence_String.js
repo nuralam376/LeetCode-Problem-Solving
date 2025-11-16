d7380e652e0f56cc3b0ed32ae03312202ef8c2a5
Nur A Alam <nuraalam376@gmail.com>
2025-11-16 10:58:02 +0600

    28. Find the Index of the First Occurrence in a String

--- Two Pointer/28.Index_First_Occurrence_String.js
+++ Two Pointer/28.Index_First_Occurrence_String.js
@@ -23,6 +23,50 @@ var strStr = function(haystack, needle) {
     return -1;
 };
 
-console.log(strStr("sadbutsad","sad"));
-console.log(strStr("leetcode", "leeto"));
-console.log(strStr("a", "a"));
+var kmpStrStr = function(haystack, needle) {
+  let n = haystack.length;
+  let m = needle.length;
+  let i = 0;
+  let j = 1;
+  let lps = [0];
+
+  while(j < m) {
+    if(needle[i] === needle[j]) {
+        lps[j] = i + 1;
+        i++;
+        j++;
+    } else {
+        if(i === 0) {
+            lps[j] = 0;
+            j++;
+        } else {
+            i = lps[i -1];
+        }
+    }
+  }
+
+  i = j = 0;
+
+  while(i < n) {
+    if(haystack[i] === needle[j]) {
+        i++;
+        j++;
+    } else {
+        if(j === 0) {
+            i++;
+        } else {
+            j = lps[j - 1];
+        }
+    }
+
+    if(j === m) {
+        return i - m;
+    }
+  }
+
+  return -1;
+};
+
+console.log(kmpStrStr("sadbutsad","sad"));
+console.log(kmpStrStr("leetcode", "leeto"));
+console.log(kmpStrStr("a", "a"));
