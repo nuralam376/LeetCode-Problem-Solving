8687bdd7996a1c2994c7ee0b46e478b2cdeb155a
Nur A Alam <nuraalam376@gmail.com>
2025-11-17 10:11:24 +0600

    15. 3 Sum

---
+++ Two Pointer/15.3Sum.js
@@ -0,0 +1,45 @@
+// 15. 3Sum
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    let ans = [];
+    for(let i = 0; i < nums.length; i++) {
+        if(i === 0 || nums[i] !== nums[i - 1]) {
+            twoSum(nums, i, ans);
+        }
+    }
+    return ans;
+};
+
+var twoSum = (arr,x,ans) => {
+    let i = x + 1;
+    let j = arr.length - 1;
+
+    while(i < j) {
+        let sum = arr[i] + arr[j] + arr[x];
+
+        if(sum > 0) {
+            j--;
+        } else if(sum < 0) {
+            i++;
+        } else {
+            ans.push(arr[i], arr[j], arr[x]);
+            i++;
+            j--;
+
+            while(i < j && arr[i] === arr[i - 1]) {
+                i++;
+            }
+        }
+    }
+};
+
+
+console.log(threeSum([-1,0,1,2,-1,-4]));
+console.log(threeSum([0,1,1]));
+console.log(threeSum([0,0,0]));
