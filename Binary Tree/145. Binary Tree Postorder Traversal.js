7ae9fc178a97083794a2a28b9acca3a685b64ee4
Nur A Alam <nuraalam376@gmail.com>
2025-12-03 12:13:29 +0600

    145. Binary Tree Postorder Traversal

---
+++ Binary Tree/145. Binary Tree Postorder Traversal.js
@@ -0,0 +1,29 @@
+// 145. Binary Tree Postorder Traversal
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+  if(!root) return [];
+  let ans = [];
+  
+  function traversal(curr) {
+    if(!curr) return;
+    traversal(curr.left);
+    traversal(curr.right);
+    ans.push(curr.val);
+  }
+
+  traversal(root);
+  return ans;
+};
